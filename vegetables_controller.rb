28a7750acf8fe2e48fd9c8167b3ec17aed5c8d8a
Alice Fung <alice.o.fung@gmail.com>
2015-07-20 11:30:20 -0400

    added comments and fix

--- vegetables_controller.rb
+++ vegetables_controller.rb
@@ -1,7 +1,8 @@
 class VegetablesController < ApplicationController
 
+  #Vegetable instead of Vegetables, since the Model is singular
   def index
-    @vegetables = Vegetables.all
+    @vegetables = Vegetable.all
   end
 
   def show
@@ -17,36 +18,51 @@ class VegetablesController < ApplicationController
     if @vegetable.save
       flash[:success] = "That sounds like a tasty vegetable!"
       redirect_to @vegetable
+    else
+      flash.now[:error] = "Aww, that veggie wasn't created."
+      render :new
     end
-    redirect_to :new
+    #should be part of if/else statement, also can just re-render and not lose the info already written and potential helpful error msgs
+    # redirect_to :new #render :new
   end
 
   def edit
-    @vegetable = Vegetable.find(whitelisted_vegetable_params)
+    #only needs id here to find this model instance
+    @vegetable = Vegetable.find(params[:id])
   end
 
   def update
-    @vegetable = Vegetable.new(whitelisted_vegetable_params)
-    if @vegetable.update
+    #do not need a new vegetable instance, just find the one that's updating
+    @vegetable = Vegetable.find(params[:id])
+    if @vegetable.update(whitelisted_vegetable_params) #needs the params here
       flash[:success] = "A new twist on an old favorite!"
       redirect_to @vegetable
     else
-      flash[:error] = "Something is rotten here..."
+      #needs a flash.now to show up now and not the next pg
+      flash.now[:error] = "Something is rotten here..."
       render :edit
     end
   end
 
   def delete
     @vegetable = Vegetable.find(params[:id])
-    @vegetable.destroy
-    flash[:success] = "That veggie is trashed."
-    redirect_to @vegetable
+    #I'll just add an if/else statement with flash msg in case of errors
+    if @vegetable.destroy
+      flash[:success] = "That veggie is trashed."
+      # redirect_to @vegetable  #it was deleted!!! won't exist
+      redirect_to vegetables_path #the index
+    else
+      flash.now[:error] = "It's a weed! That veggie is still there and not trashed yet."
+      render :edit
+    end
   end
 
   private
 
   def whitelisted_vegetable_params
-    require(:vegetable).permit(:name, :color, :rating, :latin_name)
+    #incorrect format
+    #require(:vegetable).permit(:name, :color, :rating, :latin_name)
+    params.require(:vegetable).permit(:name, :color, :rating, :latin_name)
   end
 
 end
